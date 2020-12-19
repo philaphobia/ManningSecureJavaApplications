90346493d8ff4d302f67ecfd90a86ee31908d28e
Phil Kulp <philaphobia@users.noreply.github.com>
2020-12-19 17:19:53 -0500

    Update Project1.java

--- src/com/johnsonautoparts/Project1.java
+++ src/com/johnsonautoparts/Project1.java
@@ -9,6 +9,7 @@ import java.util.*;
 import java.util.regex.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.johnsonautoparts.exception.AppException;
 import com.johnsonautoparts.logger.AppLogger;
